7ff15645f7b329b3bb6da398d03e0a73843362b3
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2026-04-02 22:41:48 -0400

    upload

---
+++ data.js
@@ -0,0 +1,274 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1-c-cha-gisu-chu-php-ra-ti-sng-gioan",
+      "name": "一、耶稣在约但河受洗",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6649957818216627,
+          "pitch": 0.08677225450899329,
+          "rotation": 0,
+          "target": "1-2-c-cha-gisu-d-tic-ci-cana"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6319923911907424,
+          "pitch": -0.17290606185455815,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">耶稣在约但河受洗</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">那时，耶稣由加里肋亚来到约但河若翰那里，为受他的洗；但若翰想要阻止他说：「我本来需要受你的洗，而你却来就我吗？」耶稣回答他说：「你暂且容许罢！因为我们应当这样，以完成全义。」于是若翰就容许了他。</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">耶稣受洗后，立时从水里上来，忽然天为他开了。他看见天主圣神有如鸽子降下，来到他上面；又有声音由天上说：「这是我的爱子，我所喜悦的。」<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(玛窦福音 3:13–17)<o:p></o:p></p></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-2-c-cha-gisu-d-tic-ci-cana",
+      "name": "二、耶稣参加加纳婚宴",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.20790684707299256,
+          "pitch": -0.008690813679965714,
+          "rotation": 0,
+          "target": "2-3-c-cha-gisu-rao-ging-nc-tri-v-ku-gi-sm-hi"
+        },
+        {
+          "yaw": 1.3359429840287653,
+          "pitch": 0.13937177954095148,
+          "rotation": 0,
+          "target": "0-1-c-cha-gisu-chu-php-ra-ti-sng-gioan"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.24373764674771792,
+          "pitch": -0.2789593316817669,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">耶稣参加加纳婚宴</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">第三天，在加里肋亚加纳有婚宴，耶稣的母亲在那里；耶稣和他的门徒也被请去赴婚宴。酒缺了，耶稣的母亲向他说：「他们没有酒了。」耶稣回答说：「女人，这于我和你有什么关系？我的时刻尚未来到。」他的母亲给仆役说：「他无论吩咐你们什么，你们就作什么。」</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">在那里放着六口石缸，是为犹太人的取洁礼用的；每口可容纳两三桶水。耶稣向仆役说：「你们把缸灌满水罢！」他们就灌满了，直到缸口。然后，耶稣给他们说：「现在你们舀出来，送给司席！」他们便送去了。司席一尝已变成酒的水──并不知是从那里来的，舀水的仆役却知道──司席便叫了新郎来，向他说：「人人都先摆上好酒，当客人都喝够了，才摆上次等的；你却把好酒保留到现在。」这是耶稣所行的第一个神迹，是在加里肋亚加纳行的；他显示了自己的光荣，他的门徒就信从了他。<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(若望福音 2:1-11)</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-3-c-cha-gisu-rao-ging-nc-tri-v-ku-gi-sm-hi",
+      "name": "三、耶稣宣讲天国福音",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.05358848048561171,
+          "pitch": -0.17051613309329028,
+          "rotation": 0,
+          "target": "3-4-c-cha-gisu-bin-hnh-trn-ni"
+        },
+        {
+          "yaw": -1.7029965550555701,
+          "pitch": 0.1012106739874703,
+          "rotation": 0,
+          "target": "1-2-c-cha-gisu-d-tic-ci-cana"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.004657148241202336,
+          "pitch": -0.4721606572718109,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">耶稣宣讲天国福音</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">若翰被监禁后，耶稣来到加里肋亚，宣讲天主的福音，说：「时期已满，天主的国临近了，你们悔改，信从福音罢！」<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(马尔谷福音 1:14-15)</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-4-c-cha-gisu-bin-hnh-trn-ni",
+      "name": "四、耶稣大博尔山显容",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7953274170598537,
+          "pitch": 0.37459110494686243,
+          "rotation": 0,
+          "target": "4-5-c-cha-gisu-lp-b-tch-thnh-th"
+        },
+        {
+          "yaw": -1.748073222986461,
+          "pitch": -0.012033693567310877,
+          "rotation": 0,
+          "target": "2-3-c-cha-gisu-rao-ging-nc-tri-v-ku-gi-sm-hi"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.71832037522912,
+          "pitch": -0.1197861019002886,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">耶稣大博尔山显容</span><o:p></o:p></p>",
+          "text":  "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">六天以后，耶稣带着伯多禄，雅各伯和他的兄弟若望，单独带领他们上了一座高山，在他们面前变了容貌：他的面貌发光有如太阳，他的衣服洁白如光。忽然，梅瑟和厄里亚也显现给他们，正在同耶稣谈论。伯多禄就开口对耶稣说：「主啊，我们在这里真好！你若愿意，我就在这里张搭三个帐棚：一个为你，一个为梅瑟，一个为厄里亚。」他还在说话的时候，忽有一片光耀的云彩遮蔽了他们，并且云中有声音说：「这是我的爱子，我所喜悦的，你们要听从他！」门徒听了，就俯伏在地，非常害怕。耶稣遂前来，抚摩他们说：「起来，不要害怕！」他们举目一看，任谁都不见了，只有耶稣独自一人。<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(玛窦福音 17:1-8)</div>"
+        }
+      ]
+    },
+    {
+      "id": "4-5-c-cha-gisu-lp-b-tch-thnh-th",
+      "name": "五、耶稣建立圣体圣事",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7996773456601955,
+          "pitch": 0.0118117757142997,
+          "rotation": 0,
+          "target": "3-4-c-cha-gisu-bin-hnh-trn-ni"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.06865999545901147,
+          "pitch": -0.5026090379846924,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">耶稣建立圣体圣事</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">他们正吃晚餐的时候，耶稣拿起饼来，祝福了，擘开递给门徒说：「你们拿去吃罢！这是我的身体。」然后，又拿起杯来，祝谢了，递给他们说：「你们都由其中喝罢！因为这是我的血，新约的血，为大众倾流，以赦免罪过。<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(玛窦福音 26:26-28)</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
